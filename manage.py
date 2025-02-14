fa99061a4d79dcb3f4ecbf54adb2bf6569e0922b
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2025-02-14 21:17:31 +0600

    All Done

--- manage.py
+++ manage.py
@@ -6,11 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopzone.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopzone_backend.settings')
->>>>>>> 65956c2 (Filtering Added)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shopzone_backend.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -22,5 +19,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
